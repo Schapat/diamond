03f73f04c1dcbcad3cdbd30639f63f99491e4654
Schapat <patrick.scharnow@gmx.de>
2021-09-25 21:24:44 +0200

    Diamond

---
+++ DiamondApp/DiamondApp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DiamondApp
+{
+    class DiamondApp
+    {
+        static void Main(string[] args)
+        {
+            Diamond(5);
+        }
+        /**
+        *   Method to create a diamond as output on the console
+        *   @param1 maximum width
+        */
+        public static void Diamond(int n)
+        {
+            int half = n / 2;
+
+            for (int i = 0; i <= half; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (half + i >= j && half - i <= j)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+                }
+                Console.Write("\n");
+            }
+
+            for (int i = half - 1; i >= 0; i--)
+            {
+                for (int j = n; j > 0; j--)
+                {
+                    if (half + 1 + i >= j && half + 1 - i <= j)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write("#");
+                    }
+                }
+                Console.Write("\n");
+            }
+
+        }
+
+    }
+}
+
